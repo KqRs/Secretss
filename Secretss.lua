17af1a098af1f45ac45bee7e7c05f3b26401bf6c
KqRs <159464375+KqRs@users.noreply.github.com>
2024-04-10 09:05:07 +0300

    Secretss.lua

--- Secretss.lua
+++ Secretss.lua
@@ -45,17 +45,12 @@ Misctab.newButton("Восстановление Энергии", "Телепор
     until game.Players.LocalPlayer.Character:FindFirstChild("Humanoid").Sit == true
 end)
 
-Misctab.newButton("Отключение Задержки Действий", "Очень крутая, но необратимая функция ", function()
-    for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
-        if v:IsA("ProximityPrompt") then
-            v["HoldDuration"] = 0
-        end
-    end
-    
-    
-    game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(function(v)
-        v["HoldDuration"] = 0
-    end)
+Misctab.newButton("TP", "SUPPLY", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1980.80334, 111.89325, 2015.25854, -0.986074865, -1.96702814e-08, 0.166301906, -1.1049198e-08, 1, 5.27651451e-08, -0.166301906, 5.0192881e-08, -0.986074865)
+end)
+
+Deliverytab.newButton("ПОЛУЧИТЬ РАКЕТУ", "ДА", function()
+    GetRocket()
 end)
 
 Deliverytab.newToggle("Сбор Булочек", "Работает только если вы Доставщик", false, function(toggleState)
@@ -64,14 +59,15 @@ Deliverytab.newToggle("Сбор Булочек", "Работает только 
     local VirtualInputManager = game:GetService("VirtualInputManager")
     if toggleState then
         while getgenv().Bun do
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Bun.CFrame + Vector3.new(3,5,9)
-            wait(0.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Bun.CFrame + Vector3.new(3,0,9)
+            wait(0.6)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
+            wait(0.1)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
-            wait(.2)
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Bun.Spawnpoint.CFrame + Vector3.new(0,-14,0)
+            wait(0.4)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2055.59521, 112.59124, 2047.5752, -0.947956324, -3.48592515e-08, -0.318400383, -9.86109061e-09, 1, -8.01235487e-08, 0.318400383, -7.28138474e-08, -0.947956324)
             wait(1.4)
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1628.72864, 387.527679, 1147.60547, 0.99978435, -5.05287652e-08, -0.0207671076, 5.07759381e-08, 1, 1.13748051e-08, 0.0207671076, -1.24268213e-08, 0.99978435) + Vector3.new(0,10,0)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1628.72864, 387.527679, 1147.60547, 0.99978435, -5.05287652e-08, -0.0207671076, 5.07759381e-08, 1, 1.13748051e-08, 0.0207671076, -1.24268213e-08, 0.99978435) + Vector3.new(0,6,0)
             wait(0.7)
         end
     end
@@ -82,12 +78,13 @@ Deliverytab.newToggle("Сбор Салата", "Работает только е
     local VirtualInputManager = game:GetService("VirtualInputManager")
     if toggleState then
         while getgenv().Lettuce do
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Lettuce.CFrame + Vector3.new(3,5,9)
-            wait(0.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Lettuce.CFrame + Vector3.new(3,0,9)
+            wait(0.6)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
+            wait(0.1)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
-            wait(.2)
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Lettuce.Spawnpoint.CFrame + Vector3.new(0,-14,0)
+            wait(0.4)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2035.70288, 112.689873, 2054.99292, -0.949388742, -3.74417652e-08, -0.314103484, -9.28101596e-09, 1, -9.11498148e-08, 0.314103484, -8.36214085e-08, -0.949388742)
             wait(1.4)
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1628.72864, 387.527679, 1147.60547, 0.99978435, -5.05287652e-08, -0.0207671076, 5.07759381e-08, 1, 1.13748051e-08, 0.0207671076, -1.24268213e-08, 0.99978435) + Vector3.new(0,10,0)
             wait(0.7)
@@ -100,12 +97,13 @@ Deliverytab.newToggle("Сбор Помидора", "Работает тольк�
     local VirtualInputManager = game:GetService("VirtualInputManager")
     if toggleState then
         while getgenv().Tomato do
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Tomato.CFrame + Vector3.new(3,5,9)
-            wait(0.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Tomato.CFrame + Vector3.new(3,0,9)
+            wait(0.6)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
+            wait(0.1)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
-            wait(.2)
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Tomato.Spawnpoint.CFrame + Vector3.new(0,-14,0)
+            wait(0.4)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(2015.69568, 112.795868, 2061.57886, -0.947974741, 3.37611681e-08, -0.318345606, 1.20004318e-08, 1, 7.0316851e-08, 0.318345606, 6.28383106e-08, -0.947974741)
             wait(1.4)
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1628.72864, 387.527679, 1147.60547, 0.99978435, -5.05287652e-08, -0.0207671076, 5.07759381e-08, 1, 1.13748051e-08, 0.0207671076, -1.24268213e-08, 0.99978435) + Vector3.new(0,10,0)
             wait(0.7)
@@ -118,12 +116,13 @@ Deliverytab.newToggle("Сбор Лука", "Работает только есл
     local VirtualInputManager = game:GetService("VirtualInputManager")
     if toggleState then
         while getgenv().Onion do
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Onion.CFrame + Vector3.new(3,5,9)
-            wait(0.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Onion.CFrame + Vector3.new(3,0,9)
+            wait(0.6)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
+            wait(0.1)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
-            wait(.2)
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Onion.Spawnpoint.CFrame + Vector3.new(0,-14,0)
+            wait(0.4)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1994.83069, 112.883392, 2068.69531, -0.953961372, 4.19368895e-09, -0.29992947, 3.55305174e-10, 1, 1.28521602e-08, 0.29992947, 1.21538974e-08, -0.953961372)
             wait(1.4)
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1628.72864, 387.527679, 1147.60547, 0.99978435, -5.05287652e-08, -0.0207671076, 5.07759381e-08, 1, 1.13748051e-08, 0.0207671076, -1.24268213e-08, 0.99978435) + Vector3.new(0,10,0)
             wait(0.7)
@@ -136,12 +135,11 @@ Deliverytab.newToggle("Сбор Огурца", "Работает только е
     local VirtualInputManager = game:GetService("VirtualInputManager")
     if toggleState then
         while getgenv().Pickle do
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Pickle.CFrame + Vector3.new(3,5,9)
-            wait(0.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Pickle.CFrame + Vector3.new(3,0,9)
+            wait(0.6)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
-            VirtualInputManager:SendKeyEvent(true, "E", false, game)
-            wait(.2)
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Pickle.Spawnpoint.CFrame + Vector3.new(0,-14,0)
+            wait(0.4)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1974.90491, 113.007843, 2076.0061, -0.937414944, 1.45005785e-08, -0.348214269, 4.80522733e-09, 1, 2.87067117e-08, 0.348214269, 2.52368526e-08, -0.937414944)
             wait(1.4)
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1628.72864, 387.527679, 1147.60547, 0.99978435, -5.05287652e-08, -0.0207671076, 5.07759381e-08, 1, 1.13748051e-08, 0.0207671076, -1.24268213e-08, 0.99978435) + Vector3.new(0,10,0)
             wait(0.7)
@@ -154,12 +152,13 @@ Deliverytab.newToggle("Сбор Сыра", "Работает только есл
     local VirtualInputManager = game:GetService("VirtualInputManager")
     if toggleState then
         while getgenv().Cheese do
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Cheese.CFrame + Vector3.new(3,5,9)
-            wait(0.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Cheese.CFrame + Vector3.new(3,0,9)
+            wait(0.6)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
+            wait(0.1)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
-            wait(.2)
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Cheese.Spawnpoint.CFrame + Vector3.new(0,-14,0)
+            wait(0.4)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1955.31152, 113.081619, 2084.10791, -0.947274864, -2.5139606e-08, -0.320422083, -4.35198499e-09, 1, -6.55918555e-08, 0.320422083, -6.07390405e-08, -0.947274864)
             wait(1.4)
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1628.72864, 387.527679, 1147.60547, 0.99978435, -5.05287652e-08, -0.0207671076, 5.07759381e-08, 1, 1.13748051e-08, 0.0207671076, -1.24268213e-08, 0.99978435) + Vector3.new(0,10,0)
             wait(0.7)
@@ -172,12 +171,13 @@ Deliverytab.newToggle("Сбор Котлет", "Работает только е
     local VirtualInputManager = game:GetService("VirtualInputManager")
     if toggleState then
         while getgenv().Patty do
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Patty.CFrame + Vector3.new(3,5,9)
-            wait(0.5)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Patty.CFrame + Vector3.new(3,0,9)
+            wait(0.6)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
+            wait(0.1)
             VirtualInputManager:SendKeyEvent(true, "E", false, game)
-            wait(.2)
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Supply.GenerationPrompts.Patty.Spawnpoint.CFrame + Vector3.new(0,-14,0)
+            wait(0.4)
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1934.93079, 113.168205, 2090.54492, -0.945509732, 2.90418054e-08, -0.325593889, 6.1233405e-09, 1, 7.14145116e-08, 0.325593889, 6.55293917e-08, -0.945509732)
             wait(1.4)
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1628.72864, 387.527679, 1147.60547, 0.99978435, -5.05287652e-08, -0.0207671076, 5.07759381e-08, 1, 1.13748051e-08, 0.0207671076, -1.24268213e-08, 0.99978435) + Vector3.new(0,10,0)
             wait(0.7)
@@ -198,6 +198,28 @@ function Holding()
     end)
 end
 Holding()
+
+function GetRocket()
+    local VirtualInputManager = game:GetService("VirtualInputManager")
+    wait(0.5)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").RuntimeTemp.SupplyRockets.CargoRocket.ExitPart.CFrame
+    wait(0.1)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+    wait(2)
+    VirtualInputManager:SendKeyEvent(true, "S", false, game)
+    wait(0.5)
+    VirtualInputManager:SendKeyEvent(false, "S", false, game)
+    wait(3)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+    VirtualInputManager:SendKeyEvent(true, "E", false, game)
+end
 
 
 
@@ -614,6 +636,7 @@ function ClearTable()
             }
         
             game:GetService("ReplicatedStorage"):WaitForChild("Kitchen"):WaitForChild("Events"):WaitForChild("CombineObjects"):FireServer(unpack(args))
+            wait(1)
         end
     end)
 end
